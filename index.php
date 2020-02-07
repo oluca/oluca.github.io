561b28a537767ea22113bd5d57cf4c86e1618028
Luca <60780902+oluca@users.noreply.github.com>
2020-02-07 13:19:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Luca's site</title>
+  </head>
+  <body>
+    <h1>Hello and welcome!</h1>
+  </body>
+</html>
